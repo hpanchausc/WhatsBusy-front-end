67bf550321a9df09c0e48c6d21b9afefdf6294a8
Tien <tiennguyen@Tiens-MacBook-Pro.local>
2020-09-15 12:42:21 -0700

    Added animation.js to display picture/gif animation on Home section image.

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,45 @@
+const animationArr = [
+    'images/home-7/Industry.png', // 5
+    '/images/home-7/Segment.png', // 10
+]
+
+// Initialize Counter.
+var count = 0
+
 // Handler for .ready() called.
 $(function() {
-    $example = $('<iframe src="https://giphy.com/embed/xUPGcoTKhfDvStjxMA" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>')
-    $(".home-section-photo")
-        .mouseenter(function() {
-            alert("Enter")
-        })
-        .mouseleave(function() {
-            alert("Left")
-        })
+    // Loop through array of animations/pictures.
+    $.each(animationArr, function( index, value ) {
+
+        // Interval to display every 5 seconds.
+        setInterval(() => {
+            // When it hits 5 seconds then display the value ===
+            if ((count / 5) === index)  {
+                if($('.overlay')) {
+                    $('.overlay').remove()
+                }
+                $('.home-section-photo').append($(`<img src=${value} class="overlay img-fluid animated fadeIn">`))
+                console.log(value)
+                count++
+            } else if (count === (animationArr.length * 5)) {
+                count = 0
+            } else {
+                count++
+            }
+            console.log(count)
+        }, 5000);
+    })
 })
 
+/*
 
+if ((count / 5) === index)  {
+                console.log(value)
+                count++
+            } else if (count === (testArr.length * 5)) {
+                count = 0
+            } else {
+                count++
+            }
+
+*/
