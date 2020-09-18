f10c14e4bd85e8c386114f61724a467382a63fcc
Tien <tiennguyen@Tiens-MacBook-Pro.local>
2020-09-18 12:29:45 -0700

    Italicize 'if' in Benchmark section.

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
     $('.tab-panel-nav').on('slide.bs.carousel', function (e) {
 
-        var $e = $(e.relatedTarget);
+        var $e = $(e.relatedTarget)
         
         var idx = $e.index();
-        console.log("IDX :  " + idx);
+        console.log("IDX :  " + idx)
         
         var itemsPerSlide = 15;
         var totalItems = $('.carousel-item').length;
         
         if (idx >= totalItems-(itemsPerSlide-1)) {
-            var it = itemsPerSlide - (totalItems - idx);
+            var it = itemsPerSlide - (totalItems - idx)
             for (var i=0; i<it; i++) {
                 // append slides to end
                 if (e.direction=="left") {
-                    $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                    $('.carousel-item').eq(i).appendTo('.carousel-inner')
                 }
                 else {
-                    $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                    $('.carousel-item').eq(0).appendTo('.carousel-inner')
                 }
             }
         }
